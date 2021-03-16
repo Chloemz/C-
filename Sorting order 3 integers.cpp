c84543b295b630368e12e9f1b29e39a9587b6479
Chloemz <57193454+Chloemz@users.noreply.github.com>
2021-03-16 17:53:30 -0400

    Update and rename HW E5.15 sort3.cpp to Sorting order 3 integers.cpp

--- Sorting order 3 integers.cpp
+++ Sorting order 3 integers.cpp
@@ -1,14 +1,12 @@
 //Name: Chloe Zambrano
-//Email: Chloe.zambrano94@myhunter.cuny.edu
-//CSCi 13500-01  HW E5.15 sort3.cpp
-//Instructor: Tong Yi
+
 //This Program swaps its three arguments to arrange them in sorted order.
 // void sort2(int& a, int& b, int&c)
 
 
-#include <iostream>   //The built-in library for input/output
-#include <algorithm> // for swap function
-using namespace std; //The names of standard definitions
+#include <iostream>   
+#include <algorithm> 
+using namespace std; 
 
  
 
